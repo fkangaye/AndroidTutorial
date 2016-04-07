41ee3fbc2e133eb450dba6a198d0141fe24e92b9
fkangaye <fkangaye@yahoo.com>
2016-04-06 17:00:51 -0700

    test

---
+++ app/src/androidTest/java/com/example/student/myfirstapp/ApplicationTest.java
@@ -0,0 +1,23 @@
+package com.example.student.myfirstapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+import android.view.View;
+
+import org.junit.Test;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+
+    @Test
+    public void test() {
+        MainActivity activity = new MainActivity();
+        activity.sendMessage(new View(getApplication()));
+
+    }
+}
